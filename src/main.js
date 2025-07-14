4a55ef60f86b8f97a8fece0a91b8e20e54352d13
Evan Schultz <e.schultz.p82@gmail.com>
2025-07-14 12:50:31 -0400

    fix: Restore full DoorKnocker app with fixed pattern engine

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
-// import DoorKnocker from './components/DoorKnocker.vue'
+import DoorKnocker from './components/DoorKnocker.vue'
 // import MinimalDoorKnocker from './components/MinimalDoorKnocker.vue'
 // import SimpleDoorKnocker from './components/SimpleDoorKnocker.vue'
-import TestPatternEngine from './components/TestPatternEngine.vue'
+// import TestPatternEngine from './components/TestPatternEngine.vue'
 import './style.css'
 
-createApp(TestPatternEngine).mount('#app')
+createApp(DoorKnocker).mount('#app')
